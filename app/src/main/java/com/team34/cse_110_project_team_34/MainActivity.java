17ddf034d8e08241e950c2c8ce8b056c8c51a8ae
Albert Ai <gsbalbert2002@gmail.com>
2023-02-08 16:09:40 -0800

    Initialized project

---
+++ app/src/main/java/com/team34/cse_110_project_team_34/MainActivity.java
@@ -0,0 +1,14 @@
+package com.team34.cse_110_project_team_34;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
