209260b2a2167505673aa50159a0995a70e13dc7
Albert Ai <gsbalbert2002@gmail.com>
2023-02-10 13:01:08 -0800

    Revert "Story 1 Task 3 + Story 4 Task 2"

--- app/src/main/java/utilities/Database.java
+++
@@ -1,8 +0,0 @@
-package utilities;
-
-import androidx.room.RoomDatabase;
-
-@androidx.room.Database(entities = {Coordinate.class}, version = 1)
-public abstract class Database extends RoomDatabase {
-    public abstract CoordinateDao getCoordinateDao();
-}
