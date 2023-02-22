6095ae2e59ee59058963bcf61d08c486fb17b262
Albert Ai <gsbalbert2002@gmail.com>
2023-02-22 14:08:47 -0800

    Fixed angle calculation

--- app/src/main/java/utilities/Calculation.java
+++ app/src/main/java/utilities/Calculation.java
@@ -6,19 +6,12 @@ public class Calculation {
     }
 
     public static float getAngle(double lat1, double long1, double lat2, double long2) {
-        lat1 = Math.toRadians(lat1);
-        long1 = Math.toRadians(long1);
-        lat2 = Math.toRadians(lat2);
-        long2 = Math.toRadians(long2);
+        double dLat = lat2 - lat1;
+        double dLng = long1 - long2;
 
-        double dLong = (long2 - long1);
+        double radsFromX = Math.atan2(dLat, dLng);
+        double degsFromN = Math.toDegrees(radsFromX) - 90;
 
-        double y = Math.sin(dLong) * Math.cos(lat2);
-        double x = Math.cos(lat1) * Math.sin(lat2) - Math.sin(lat1) * Math.cos(lat2) * Math.cos(dLong);
-        double angle = Math.atan2(y, x);
-
-        return (float) (Math.toDegrees(angle) + 360) % 360;
+        return (float) degsFromN;
     }
-
-
 }
