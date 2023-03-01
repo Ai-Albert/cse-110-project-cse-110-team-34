85738dfad71649aa5b5e4bccdecf717200077f33
Victor Siu <siuleungjing@gmail.com>
2023-03-01 15:30:45 -0800

    Add user uids

--- app/src/main/java/utilities/Calculation.java
+++ app/src/main/java/utilities/Calculation.java
@@ -1,5 +1,7 @@
 package utilities;
 
+import java.util.Random;
+
 public class Calculation {
     public static float getCompassRotation(float deviceOrientation) {
         return 360 - (float) Math.toDegrees(deviceOrientation);
@@ -18,6 +20,19 @@ public class Calculation {
         double angle = Math.atan2(y, x);
 
         return (float) (Math.toDegrees(angle) + 360) % 360;
+    }
+
+    public static String getRandomUID(int length) {
+        int leftLimit = 48; // numeral '0'
+        int rightLimit = 122; // letter 'z'
+        int targetStringLength = 10;
+        Random random = new Random();
+
+        return random.ints(leftLimit, rightLimit + 1)
+                .filter(i -> (i <= 57 || i >= 65) && (i <= 90 || i >= 97))
+                .limit(targetStringLength)
+                .collect(StringBuilder::new, StringBuilder::appendCodePoint, StringBuilder::append)
+                .toString();
     }
 
 
---
+++ app/src/main/java/utilities/User.java
@@ -0,0 +1,60 @@
+package utilities;
+
+import androidx.annotation.Nullable;
+import androidx.room.ColumnInfo;
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
+
+@Entity
+public class User {
+    private static int nextId = 0;
+
+    @PrimaryKey
+    public long id;
+
+    @ColumnInfo(name = "name")
+    public String name;
+
+    @ColumnInfo(name = "longitude")
+    public double longitude;
+
+    @ColumnInfo(name = "latitude")
+    public double latitude;
+
+    @ColumnInfo(name = "UID")
+    public String uid;
+
+    public User(String name, double longitude, double latitude) {
+        this.id = nextId;
+        this.name = name;
+        this.longitude = longitude;
+        this.latitude = latitude;
+        this.uid = Calculation.getRandomUID(10);
+        nextId++;
+    }
+
+    public boolean equals(User user) {
+        return this.uid == user.uid;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getLongitude() {
+        return longitude;
+    }
+
+    public void setLongitude(double longitude) {
+        this.longitude = longitude;
+    }
+
+    public double getLatitude() {
+        return latitude;
+    }
+
+    public void setLatitude(double latitude) {
+        this.latitude = latitude;
+    }
+
+}
