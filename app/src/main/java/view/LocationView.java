7d9f8ed2d808eeb830ef7af5991ec8a5b3ab44ef
Victor Siu <siuleungjing@gmail.com>
2023-03-15 13:24:29 -0700

    Add time since updated

--- app/src/main/java/view/LocationView.java
+++ app/src/main/java/view/LocationView.java
@@ -11,6 +11,8 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import com.team34.cse_110_project_team_34.R;
 
+import org.w3c.dom.Text;
+
 import java.time.Instant;
 import java.util.Collections;
 import java.util.List;
@@ -23,10 +25,13 @@ public class LocationView {
     public final TextView nameView;
     public final ImageView statusView;
 
+    public final TextView timeView;
+
     public LocationView(User user, View view) {
         this.itemView = view;
         this.nameView = itemView.findViewById(R.id.location_name);
         this.statusView = itemView.findViewById(R.id.indicator);
+        this.timeView = itemView.findViewById(R.id.last_live);
         update(user);
     }
 
@@ -36,8 +41,16 @@ public class LocationView {
         nameView.setText(user.getName());
         if (user.getLastUpdated() + 60 >= Instant.now().getEpochSecond()) {
             statusView.setImageResource(R.drawable.green_indicator);
+            timeView.setVisibility(View.INVISIBLE);
         } else {
             statusView.setImageResource(R.drawable.red_indicator);
+            long seconds_since_seen = Instant.now().getEpochSecond() - user.getLastUpdated();
+            if (seconds_since_seen < 3600) {
+                timeView.setText(seconds_since_seen / 60 + "m");
+            } else {
+                timeView.setText(seconds_since_seen / 3600 + "h");
+            }
+            timeView.setVisibility(View.VISIBLE);
         }
     }
 }
